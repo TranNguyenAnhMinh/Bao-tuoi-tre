e5a635e82dd0fc518eb090b3f504815478905a43
Trần Nguyễn Ánh Minh <112561520+TranNguyenAnhMinh@users.noreply.github.com>
2022-09-19 08:07:45 +0700

    add pic

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ var i = 0;
 
 function changeImage() {
 
-    var imgs = ["./images/shoppe.png", "./images/charles.png", "./images/cv19.jpg"];
+    var imgs = ["./images/shoppe.png", "./images/qclzd2.png", "./images/devc.jpg"];
     document.getElementById('sliders').src = imgs[i];
     i++;
     if (i == 3) {
         i = 0;
     }
-
 }
-setInterval(changeImage, 1000);
+setInterval(changeImage, 500);
